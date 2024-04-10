2bc4cea57f35cabe7b74c87692733c72ab0a8986
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-04-10 20:54:43 +0200

    feat: create the useModal Composable

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,17 @@
+<script setup lang="ts">
+import { useModal } from "../composables/modal";
+
+const modal = useModal();
+</script>
 <template>
   <div class="navbar">
     <div class="navbar-end">
       <div class="buttons">
+        <button class="button" @click="modal.showModal()">Sign Up</button>
         <RouterLink to="/posts/new" class="button">New Post</RouterLink>
       </div>
     </div>
   </div>
+
+  <Teleport to="#modal"> content rendered </Teleport>
 </template>
