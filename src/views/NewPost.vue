8ceeb059d270cadbd249f10382f3549ba1d2b95b
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-04-16 11:47:11 +0200

    feat: added Associating Posts with Users

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -2,10 +2,18 @@
 import { DateTime } from "luxon";
 import PostWriter from "../components/PostWriter.vue";
 import { TimelinePost } from "../posts";
+import { useUsers } from "../stores/users";
+
+const usersStore = useUsers();
+
+if (!usersStore.currentUserId) {
+  throw Error("User not found");
+}
 
 const post: TimelinePost = {
   id: "-1",
   title: "Title",
+  authorId: usersStore.currentUserId,
   created: DateTime.now(),
   markdown: "## Title",
   html: "<h2>title</h2>",
